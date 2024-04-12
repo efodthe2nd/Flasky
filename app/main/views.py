f4c9769ab40868dfdc2555e46d6bff19f7235140
efodthe2nd <davidefod@gmail.com>
2024-04-12 17:45:02 +0100

    comments addition

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, session, redirect, url_for, flash, abort, request, current_app, make_response
 from datetime import datetime
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
-from ..models import User, Role, Permission, Post
+from ..models import User, Role, Permission, Post, Comment
 from ..email import send_email
 from flask_login import current_user, login_required
 from ..decorators import admin_required, permission_required
@@ -175,3 +175,20 @@ def show_followed():
   resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
   #resp.set_cookie('followed_user_ids', ','.join(map(str, followed_user_ids)), max_age=30*24*60*60)
   return resp
+
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+def post(id):
+  post = Post.query.get_or_404(id)
+  form = CommentForm()
+  if form.validate_on_submit():
+    comment = Comment(body=form.body.data, post=post, author=current_user._get_current_object())
+    db.session.add(comment)
+    db.session.commit()
+    flash('Your comment has been published.')
+    return redirect(url_for('.post', id=post.id, pagee=-1))
+  page = request.args.get('page', 1, type=int)
+  if page == -1:
+    page = (post.comments.count() - 1) // current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+  pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'], error_out=False)
+  comments = pagination.items
+  return render_template('post.html', posts=[post], form=form, comments=comments, pagination=pagination)
--- app/models.py
+++ app/models.py
@@ -91,6 +91,7 @@ class User(UserMixin, db.Model):
   posts = db.relationship('Post', backref='author', lazy='dynamic')
   followed = db.relationship('Follow', foreign_keys=[Follow.follower_id],backref=db.backref('follower', lazy='joined'), lazy='dynamic', cascade='all, delete-orphan')
   followers = db.relationship('Follow', foreign_keys=[Follow.followed_id], backref=db.backref('followed', lazy='joined'), lazy='dynamic', cascade='all, delete-orphan')
+  comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
   def __init__(self, **kwargs):
     super(User, self).__init__(**kwargs)
@@ -203,6 +204,7 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     body_html = db.Column(db.Text)
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
@@ -236,3 +238,19 @@ class Post(db.Model):
     def __repr__(self):
         return f'<Post {self.body[:20]}...>'
     
+class Comment(db.Model):
+  _tablename_ = 'comments'
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.Text)
+  body_html = db.Column(db.Text)
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  disabled = db.Column(db.Boolean)
+  author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+  @staticmethod
+  def on_changed_body(target, value, oldvalue, initiator):
+    allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i', 'strong']
+    target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
